602b7204e489ba7b467d7a238d85d266c8406447
John Hallat <jhallat@gmail.com>
2020-04-12 13:34:13 -0400

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { WorkspaceComponent } from './workspace/workspace.component';
+import { ElementComponent } from './element/element.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WorkspaceComponent,
+    ElementComponent
   ],
   imports: [
     BrowserModule
